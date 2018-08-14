64907675f50572d1ccdecd01c99123a1873be453
Vito_Zhu <zyf527@vip.qq.com>
2018-08-14 17:29:01 +0800

    1728

---
+++ week-04/day-2/fetch/fetch.js
@@ -0,0 +1,51 @@
+const btn = document.querySelector('button');    //eslint-disable-line
+const paragraph = document.querySelector('p');   //eslint-disable-line
+
+btn.addEventListener('click', () => {
+  fetch('http://api.icndb.com/jokes/random')
+    .then(function (response) {
+      return response.json();
+    }, function (error) {
+      console.log(error);
+    })
+    .then(function (myJson) {
+      console.log(myJson.value.joke);
+      view(myJson.value.joke);
+    });
+});
+function view(text) {
+  paragraph.innerText = text;
+}
+
+
+
+
+
+
+
+
+// function ajax(url, method) {
+//   const promise = new Promise((resolve, reject) => {
+//     const httpRequest = new XMLHttpRequest();
+//     function handler() {
+//       if (this.readyState !== 4) {
+//         return;
+//       }
+//       if (this.status === 200) {
+//         resolve('functional');
+//       } else {
+//         reject(new Error(this.statusText));
+//       }
+//     }
+//     httpRequest.open(method, url);
+//     httpRequest.onreadystatechange = handler;
+//     httpRequest.onload = () => {
+//       const responseData = JSON.parse(httpRequest.responseText);
+//       console.log(responseData);
+//     };
+//     httpRequest.setRequestHeader('Content-Type', 'application/json');
+//     httpRequest.send();
+//   });
+
+//   return promise;
+// }
